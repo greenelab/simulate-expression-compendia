c43aa0e23815ed9c739f35be82caefa2a3deeede
ajlee21 <alexjlee.21@gmail.com>
2019-08-23 09:43:01 -0400

    update scripts after running different scripts

--- scripts/experiment_2/nbconverted/4_similarity_analysis_viz.py
+++ scripts/experiment_2/nbconverted/4_similarity_analysis_viz.py
@@ -81,15 +81,15 @@ batch_dir = os.path.join(
 # In[5]:
 
 
-get_ipython().run_cell_magic('time', '', '\nall_data_df = pd.DataFrame()\n\nfor i in num_batches:\n    print(\'Plotting PCA of 1 batch vs {} batches...\'.format(i))\n    \n    # Get batch 1 data\n    batch_1_file = os.path.join(\n        batch_dir,\n        "Batch_1.txt.xz")\n\n    batch_1 = pd.read_table(\n        batch_1_file,\n        header=0,\n        index_col=0,\n        sep=\'\\t\')\n\n    # Simulated data with all samples in a single batch\n    original_data_df =  batch_1\n    \n    # Add grouping column for plotting\n    original_data_df[\'group\'] = \'batch_1\'\n    \n    # Get data with additional batch effects added\n    batch_other_file = os.path.join(\n        batch_dir,\n        "Batch_"+str(i)+".txt.xz")\n\n    batch_other = pd.read_table(\n        batch_other_file,\n        header=0,\n        index_col=0,\n        sep=\'\\t\')\n    \n    # Simulated data with i batch effects\n    batch_data_df =  batch_other\n    \n    # Add grouping column for plotting\n    batch_data_df[\'group\'] = "batch_{}".format(i)\n    \n    ## Concatenate datasets together\n    combined_data_df = pd.concat([original_data_df, batch_data_df])\n    print(combined_data_df.shape)\n    \n    ## PCA projection\n    pca = PCA(n_components=2)\n\n    # Use trained model to encode expression data into SAME latent space\n    combined_data_numeric_df = combined_data_df.drop([\'group\'], axis=1)\n    combined_data_PCAencoded = pca.fit_transform(combined_data_numeric_df)\n\n\n    combined_data_PCAencoded_df = pd.DataFrame(combined_data_PCAencoded,\n                                               index=combined_data_df.index,\n                                               columns=[\'PC1\', \'PC2\']\n                                              )\n    \n    # Add batch labels for the dataset that each sample belongs to (i.e. how many batches were added)\n    combined_data_PCAencoded_df[\'group\'] = combined_data_df[\'group\']\n    \n    # Add column that designates which Batch effect comparision\n    combined_data_PCAencoded_df[\'num_batches\'] = str(i)\n    \n    # Concatenate all dataframes from each batch effect together\n    all_data_df = pd.concat([all_data_df, combined_data_PCAencoded_df])\n    \n    \n    # Plot\n    print(ggplot(combined_data_PCAencoded_df, aes(x=\'PC1\', y=\'PC2\')) \\\n          + geom_point(aes(color=\'group\'), alpha=0.4) \\\n          + xlab(\'PC1\') \\\n          + ylab(\'PC2\') \\\n          + ggtitle(\'Batch 1 and Batch {}\'.format(i))\n         )')
+get_ipython().run_cell_magic('time', '', '\nall_data_df = pd.DataFrame()\n\nfor i in num_batches:\n    print(\'Plotting PCA of 1 batch vs {} batches...\'.format(i))\n    \n    # Get batch 1 data\n    batch_1_file = os.path.join(\n        batch_dir,\n        "Batch_1.txt.xz")\n\n    batch_1 = pd.read_table(\n        batch_1_file,\n        header=0,\n        index_col=0,\n        sep=\'\\t\')\n\n    # Simulated data with all samples in a single batch\n    original_data_df =  batch_1.copy()\n    \n    # Add grouping column for plotting\n    original_data_df[\'group\'] = \'batch_1\'\n    \n    # Get data with additional batch effects added\n    batch_other_file = os.path.join(\n        batch_dir,\n        "Batch_"+str(i)+".txt.xz")\n\n    batch_other = pd.read_table(\n        batch_other_file,\n        header=0,\n        index_col=0,\n        sep=\'\\t\')\n    \n    # Simulated data with i batch effects\n    batch_data_df =  batch_other\n    \n    # Add grouping column for plotting\n    batch_data_df[\'group\'] = "batch_{}".format(i)\n    \n    ## Concatenate datasets together\n    combined_data_df = pd.concat([original_data_df, batch_data_df])\n    print(combined_data_df.shape)\n    \n    ## PCA projection\n    pca = PCA(n_components=2)\n\n    # Use trained model to encode expression data into SAME latent space\n    combined_data_numeric_df = combined_data_df.drop([\'group\'], axis=1)\n    combined_data_PCAencoded = pca.fit_transform(combined_data_numeric_df)\n\n\n    combined_data_PCAencoded_df = pd.DataFrame(combined_data_PCAencoded,\n                                               index=combined_data_df.index,\n                                               columns=[\'PC1\', \'PC2\']\n                                              )\n    \n    # Add batch labels for the dataset that each sample belongs to (i.e. how many batches were added)\n    combined_data_PCAencoded_df[\'group\'] = combined_data_df[\'group\']\n    \n    # Add column that designates which Batch effect comparision\n    combined_data_PCAencoded_df[\'num_batches\'] = str(i)\n    \n    # Concatenate all dataframes from each batch effect together\n    all_data_df = pd.concat([all_data_df, combined_data_PCAencoded_df])\n    \n    \n    # Plot\n    print(ggplot(combined_data_PCAencoded_df, aes(x=\'PC1\', y=\'PC2\')) \\\n          + geom_point(aes(color=\'group\'), alpha=0.4) \\\n          + xlab(\'PC1\') \\\n          + ylab(\'PC2\') \\\n          + ggtitle(\'Batch 1 and Batch {}\'.format(i))\n         )')
 
 
-# In[20]:
+# In[6]:
 
 
 # Permute simulated data
 shuffled_simulated_arr = []
-num_samples = batch_1.drop(['group']).shape[0]
+num_samples = batch_1.shape[0] ##can remove drop now?
 
 for i in range(num_samples):
     row = list(batch_1.values[i])
@@ -98,41 +98,52 @@ for i in range(num_samples):
 
 shuffled_simulated_data = pd.DataFrame(shuffled_simulated_arr, 
                                        index=batch_1.index,
-                                       columns=batch_1.drop(['group']).columns)
+                                       columns=batch_1.columns)
 shuffled_simulated_data.head()
 
 
-# In[22]:
-
-
-batch_1.head()
-
-
-# In[21]:
+# In[7]:
 
 
 # PCA of permuted dataset (Negative control)
-shuffled_data_PCAencoded = pca.fit_transform(shuffled_simulated_data)
+
+# Add batch labels for the dataset that each sample belongs to (i.e. how many batches were added)
+shuffled_simulated_data['group'] = "permuted"
+
+# Add to dataframe
+input_vs_permuted_df = pd.concat([original_data_df, shuffled_simulated_data])
+
+
+input_vs_permuted = input_vs_permuted_df.drop(['group'], axis=1)
+shuffled_data_PCAencoded = pca.fit_transform(input_vs_permuted)
 
 
 shuffled_data_PCAencoded_df = pd.DataFrame(shuffled_data_PCAencoded,
-                                           index=shuffled_simulated_data.index,
+                                           index=input_vs_permuted_df.index,
                                            columns=['PC1', 'PC2']
                                           )
 
 # Add batch labels for the dataset that each sample belongs to (i.e. how many batches were added)
-shuffled_data_PCAencoded_df['group'] = "permuted"
+shuffled_data_PCAencoded_df['group'] = input_vs_permuted_df['group']
 
-# Add column that designates which Batch effect comparision
-shuffled_data_PCAencoded_df['num_batches'] = "permuted"
 
-# Add to dataframe
-all_data_df = pd.concat([all_data_df, shuffled_data_PCAencoded_df])
+# In[8]:
+
+
+shuffled_simulated_data.head()
 
 
 # In[9]:
 
 
 # Plot
-ggplot(all_data_df, aes(x='PC1', y='PC2')) + geom_point(aes(color='group'), alpha=0.3) + facet_wrap('~num_batches') + xlab('PC1') + ylab('PC2') + ggtitle('Batch 1 and Batch {}'.format(i))
+ggplot(all_data_df, aes(x='PC1', y='PC2')) + geom_point(aes(color='group'), alpha=0.3) + facet_wrap('~num_batches') + xlab('PC1') + ylab('PC2') + ggtitle('PCA of batch 1 vs batch x')
+
+
+# In[10]:
+
+
+# Plot
+print(ggplot(shuffled_data_PCAencoded_df, aes(x='PC1', y='PC2'))       + geom_point(aes(color='group'), alpha=0.4)       + xlab('PC1')       + ylab('PC2')       + ggtitle('Simulated vs Permuated')
+     )
 
