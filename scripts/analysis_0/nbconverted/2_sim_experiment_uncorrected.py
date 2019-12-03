5d52ad5b394deb15203af4e162be39b289b19348
ajlee21 <alexjlee.21@gmail.com>
2019-12-03 17:01:39 -0500

    rename files for analysis 0

---
+++ scripts/analysis_0/nbconverted/2_sim_experiment_uncorrected.py
@@ -0,0 +1,124 @@
+
+# coding: utf-8
+
+# # Simulation experiment 
+# 
+# Run entire simulation experiment multiple times to generate confidence interval
+
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+from joblib import Parallel, delayed
+import multiprocessing
+import sys
+import os
+import pandas as pd
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+sys.path.append("../")
+from functions import pipelines
+
+from numpy.random import seed
+randomState = 123
+seed(randomState)
+
+
+# In[2]:
+
+
+# Output files
+local_dir = "/home/alexandra/Documents/"
+
+similarity_uncorrected_file = os.path.join(
+    local_dir,
+    "Data",
+    "Batch_effects",
+    "output",
+    "analysis_0_similarity_uncorrected.pickle")
+
+ci_uncorrected_file = os.path.join(
+    local_dir,
+    "Data",
+    "Batch_effects",
+    "output",
+    "analysis_0_ci_uncorrected.pickle")
+
+
+# In[3]:
+
+
+# Run multiple simulations - uncorrected
+iterations = range(10) 
+num_cores = 5
+results = Parallel(n_jobs=num_cores, verbose=100)(delayed(pipelines.simple_simulation_experiment_uncorrected)() for i in iterations)
+
+
+# In[4]:
+
+
+# Concatenate output dataframes
+all_svcca_scores = pd.DataFrame()
+
+for i in iterations:
+    all_svcca_scores = pd.concat([all_svcca_scores, results[i][1]], axis=1)
+
+all_svcca_scores
+
+
+# In[1]:
+
+
+# Get median for each row (number of experiments)
+mean_scores = all_svcca_scores.mean(axis=1).to_frame()
+mean_scores.columns = ['score']
+mean_scores
+
+
+# In[ ]:
+
+
+# Get standard dev for each row (number of experiments)
+import math
+std_scores = (all_svcca_scores.std(axis=1)/math.sqrt(10)).to_frame()
+std_scores.columns = ['score']
+std_scores
+
+
+# In[ ]:
+
+
+# Get confidence interval for each row (number of experiments)
+# z-score for 95% confidence interval 
+err = std_scores*1.96
+
+
+# In[ ]:
+
+
+# Get boundaries of confidence interval
+ymax = mean_scores + err
+ymin = mean_scores - err
+
+ci = pd.concat([ymin, ymax], axis=1)
+ci.columns = ['ymin', 'ymax']
+ci
+
+
+# In[ ]:
+
+
+mean_scores
+
+
+# In[ ]:
+
+
+# Pickle dataframe of mean scores scores for first run, interval
+mean_scores.to_pickle(similarity_uncorrected_file)
+ci.to_pickle(ci_uncorrected_file)
+
