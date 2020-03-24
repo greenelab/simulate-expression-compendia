d5acf1687043fb4f8e1342e9fe9b169008e9d37e
ajlee21 <alexjlee.21@gmail.com>
2020-03-24 11:09:20 -0400

    update comments on nb

---
+++ Pseudo_experiments/nbconverted/create_heatmap.py
@@ -0,0 +1,296 @@
+
+# coding: utf-8
+
+# ## Visualize heatmaps of differentially expressed genes
+# 
+# Visualize a heatmap of the differentially expressed genes using the original E-GEOD-51409 expression data versus the simulated expression data for the same experiment
+
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+import os
+import sys
+import ast
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import random
+import glob
+from sklearn import preprocessing
+
+sys.path.append("../")
+from functions import utils
+import generate_labeled_data
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+from numpy.random import seed
+randomState = 123
+seed(randomState)
+
+
+# In[2]:
+
+
+# Read in config variables
+config_file = os.path.abspath(os.path.join(os.getcwd(),"../configs", "config_Pa_experiment_limma.tsv"))
+params = utils.read_config(config_file)
+
+
+# In[3]:
+
+
+# Load parameters
+local_dir = params["local_dir"]
+experiment_id = 'E-GEOD-51409'
+
+
+# In[4]:
+
+
+# Input files
+# File containing expression data from template experiment
+selected_original_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "selected_original_data_"+experiment_id+"_example.txt")
+
+selected_simulated_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "selected_simulated_data_"+experiment_id+"_example.txt")
+
+selected_control_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "selected_control_data_"+experiment_id+"_example.txt")
+
+# Files containing DE summary statistics
+DE_stats_original_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "output_original",
+    "DE_stats_original_data_"+experiment_id+"_example.txt")
+
+DE_stats_simulated_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "output_simulated",
+    "DE_stats_simulated_data_"+experiment_id+"_example.txt")
+
+DE_stats_control_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "output_control",
+    "DE_stats_control_data_"+experiment_id+"_example.txt")
+
+
+# In[5]:
+
+
+# Output files
+base_dir = os.path.abspath(
+  os.path.join(
+      os.getcwd(), "../")) 
+
+heatmap_original_file = os.path.join(
+    base_dir,
+    "Pseudomonas",
+    "results",
+    "DE_heatmap_original_"+experiment_id+"_example.png")
+
+heatmap_simulated_file = os.path.join(
+    base_dir,
+    "Pseudomonas",
+    "results",
+    "DE_heatmap_simulated_"+experiment_id+"_example.png")
+
+heatmap_control_file = os.path.join(
+    base_dir,
+    "Pseudomonas",
+    "results",
+    "DE_heatmap_control_"+experiment_id+"_example.png")
+
+original_sign_DEG_file = os.path.join(
+    base_dir,
+    "Pseudomonas",
+    "results",
+    "sign_DEG_original_"+experiment_id+"_example.txt")
+
+simulated_sign_DEG_file = os.path.join(
+    base_dir,
+    "Pseudomonas",
+    "results",
+    "sign_DEG_simulated_"+experiment_id+"_example.txt")
+
+control_sign_DEG_file = os.path.join(
+    base_dir,
+    "Pseudomonas",
+    "results",
+    "sign_DEG_control_"+experiment_id+"_example.txt")
+
+
+# In[6]:
+
+
+# Read data
+selected_original_data = pd.read_table(
+    selected_original_data_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+selected_simulated_data = pd.read_table(
+    selected_simulated_data_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+selected_control_data = pd.read_table(
+    selected_control_data_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+
+DE_stats_original_data = pd.read_table(
+    DE_stats_original_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+DE_stats_simulated_data = pd.read_table(
+    DE_stats_simulated_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+DE_stats_control_data = pd.read_table(
+    DE_stats_control_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+DE_stats_simulated_data.head()
+
+
+# In[7]:
+
+
+DE_stats_control_data.head()
+
+
+# In[8]:
+
+
+# Experiment-perserving-simulated experiment
+# Get DEGs to display in heatmap
+# Get the number of genes that adjusted p-value < 0.05 AND log2FC > 1
+
+sign_DEG_simulated = DE_stats_simulated_data[
+    (abs(DE_stats_simulated_data['logFC'])>1) & (DE_stats_simulated_data['adj.P.Val']<0.05)]
+print(sign_DEG_simulated.shape[0])
+
+# Sort significant DEGs and select top 30 genes
+sign_DEG_simulated.sort_values(by=['adj.P.Val'])
+sign_DEG_simulated = sign_DEG_simulated.iloc[0:30,]
+
+sign_DEG_simulated.to_csv(
+        simulated_sign_DEG_file, float_format='%.3f', sep='\t')
+
+sign_DEG_simulated.head(10)
+
+
+# In[9]:
+
+
+DE_stats_original_data.head()
+
+
+# In[10]:
+
+
+# Original experiment
+# Get DEGs to display in heatmap
+# Get the number of genes that adjusted p-value < 0.05 and log2FC > 1
+
+sign_DEG_original = DE_stats_original_data[
+    (abs(DE_stats_original_data['logFC'])>1) & (DE_stats_original_data['adj.P.Val']<0.05)]
+print(sign_DEG_original.shape[0])
+
+# Sort significant DEGs and select top 30 genes
+sign_DEG_original.sort_values(by=['adj.P.Val'])
+sign_DEG_original = sign_DEG_original.iloc[0:14,]
+
+
+sign_DEG_original.to_csv(
+        original_sign_DEG_file, float_format='%.3f', sep='\t')
+
+sign_DEG_original.head(10)
+
+
+# In[11]:
+
+
+# Randomly-sampled-simulated experiment
+# Get DEGs to display in heatmap
+# Get the number of genes that adjusted p-value < 0.05 AND log2FC > 1
+
+sign_DEG_control = DE_stats_control_data[
+    (abs(DE_stats_control_data['logFC'])>1) & (DE_stats_control_data['adj.P.Val']<0.05)]
+print(sign_DEG_control.shape[0])
+
+if sign_DEG_control.shape[0] == 0:
+    # Reset data
+    sign_DEG_control = DE_stats_control_data
+    
+    # Sort significant DEGs and select top 30 genes
+    sign_DEG_control.sort_values(by=['adj.P.Val'])
+    sign_DEG_control = sign_DEG_control.iloc[0:14,]
+
+sign_DEG_control.to_csv(
+        control_sign_DEG_file, float_format='%.3f', sep='\t')
+
+sign_DEG_control.head(10)
+
+
+# In[12]:
+
+
+# Get gene ids for significant DEGs
+original_gene_ids = list(sign_DEG_original.index)
+sim_gene_ids = list(sign_DEG_simulated.index)
+control_gene_ids = list(sign_DEG_control.index)
+
+
+# In[13]:
+
+
+# Plot original data
+selected_original_DEG_data = selected_original_data[original_gene_ids]
+sns.set(style="ticks", context="talk")
+f = sns.clustermap(selected_original_DEG_data.T, cmap="viridis")
+f.savefig(heatmap_original_file, dpi=500)
+
+
+# In[14]:
+
+
+# Plot simulated
+selected_simulated_DEG_data = selected_simulated_data[sim_gene_ids]
+f = sns.clustermap(selected_simulated_DEG_data.T, cmap="viridis")
+f.savefig(heatmap_simulated_file, dpi=500)
+
+
+# In[15]:
+
+
+# Plot control
+selected_control_DEG_data = selected_control_data[control_gene_ids]
+f = sns.clustermap(selected_control_DEG_data.T, cmap="viridis")
+f.savefig(heatmap_control_file, dpi=500)
+
---
+++ Pseudo_experiments/nbconverted/generate_E_GEOD_51409_template_experiment.py
@@ -0,0 +1,310 @@
+
+# coding: utf-8
+
+# # Create pseudo experiments using simulated compendia
+# 
+# This notebook generates new pseudo-experiments using the experiment-preserving approach in the experiment level simulation. In this simulation we are preserving the experiment type but not the actual experiment so the relationship between samples within an experiment are preserved but the genes that are expressed will be different (module ```simulate_compendium``` in ```functions/generate_data_parallel.py```).
+
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+import os
+import sys
+import ast
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import random
+import glob
+from sklearn import preprocessing
+
+sys.path.append("../")
+from functions import utils
+import generate_labeled_data
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+from numpy.random import seed
+randomState = 123
+seed(randomState)
+
+
+# In[2]:
+
+
+# Read in config variables
+config_file = os.path.abspath(os.path.join(os.getcwd(),"../configs", "config_Pa_experiment_limma.tsv"))
+params = utils.read_config(config_file)
+
+
+# In[3]:
+
+
+# Load parameters
+num_runs = 100
+dataset_name = params["dataset_name"]
+num_simulated_experiments = params["num_simulated_experiments"]
+NN_architecture = params["NN_architecture"]
+local_dir = params["local_dir"]
+
+
+# In[4]:
+
+
+# Input files
+base_dir = os.path.abspath(
+  os.path.join(
+      os.getcwd(), "../"))    # base dir on repo
+
+# Load experiment id file
+# Contains ALL experiment ids
+experiment_ids_file = os.path.join(
+    base_dir,
+    dataset_name,
+    "data",
+    "metadata",
+    "experiment_ids.txt")
+
+normalized_data_file = os.path.join(
+    base_dir,
+    dataset_name,
+    "data",
+    "input",
+    "train_set_normalized.pcl")
+
+original_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "Pa_compendium_02.22.2014.pcl")
+
+mapping_file = os.path.join(
+    base_dir,
+    dataset_name,
+    "data",
+    "metadata",
+    "sample_annotations.tsv")
+
+
+# ## Generate simulated data with labels
+# 
+# Simulate a compendia by experiment and label each new sample with the experiment id that it originated from
+
+# In[5]:
+
+
+# Load experiment id file
+# Contains ALL experiment ids
+base_dir = os.path.abspath(
+  os.path.join(
+      os.getcwd(), "../"))    # base dir on repo
+
+experiment_ids_file = os.path.join(
+    base_dir,
+    dataset_name,
+    "data",
+    "metadata",
+    "experiment_ids.txt")
+
+
+# In[6]:
+
+
+# Generate simulated data
+# Generate simulated data
+simulated_labeled_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "simulated_data_labeled.txt.xz")
+if not Path(simulated_labeled_data_file).exists():
+    generate_labeled_data.simulate_compendium_labeled(experiment_ids_file, 
+                                                      num_simulated_experiments,
+                                                      normalized_data_file,
+                                                      NN_architecture,
+                                                      dataset_name,
+                                                      local_dir,
+                                                      base_dir)
+
+
+# ## Process data
+
+# In[7]:
+
+
+# Load simulated data
+simulated_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "simulated_data_labeled.txt.xz")
+
+
+# In[8]:
+
+
+# Read data
+original_data = pd.read_table(
+    original_data_file,
+    header=0,
+    sep='\t',
+    index_col=0).T
+
+simulated_data = pd.read_table(
+    simulated_data_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+print(original_data.shape)
+print(simulated_data.shape)
+
+
+# In[9]:
+
+
+original_data.head(5)
+
+
+# In[10]:
+
+
+simulated_data.head(5)
+
+
+# In[11]:
+
+
+# 0-1 normalize per gene
+scaler = preprocessing.MinMaxScaler()
+original_data_scaled = scaler.fit_transform(original_data)
+original_data_scaled_df = pd.DataFrame(original_data_scaled,
+                                columns=original_data.columns,
+                                index=original_data.index)
+
+original_data_scaled_df.head(5)
+
+
+# In[12]:
+
+
+# Re-scale simulated data back into the same range as the original data
+simulated_data_numeric = simulated_data.drop(columns=['experiment_id'])
+simulated_data_scaled = scaler.inverse_transform(simulated_data_numeric)
+
+simulated_data_scaled_df = pd.DataFrame(simulated_data_scaled,
+                                columns=simulated_data_numeric.columns,
+                                index=simulated_data_numeric.index)
+
+simulated_data_scaled_df['experiment_id'] = simulated_data['experiment_id']
+simulated_data_scaled_df.head(5)
+
+
+# In[13]:
+
+
+# Read in metadata
+metadata = pd.read_table(
+    mapping_file, 
+    header=0, 
+    sep='\t', 
+    index_col=0)
+
+metadata.head()
+
+
+# In[14]:
+
+
+map_experiment_sample = metadata[['sample_name', 'ml_data_source']]
+map_experiment_sample.head()
+
+
+# # Template experiment E-GEOD-51409
+# 
+# This experiment measures the transcriptome of *P. aeruginosa* under two different growth conditions: 28 degrees and 37 degress.
+
+# In[15]:
+
+
+# Get experiment id
+experiment_id = 'E-GEOD-51409'
+
+
+# In[16]:
+
+
+# Get original samples associated with experiment_id
+selected_mapping = map_experiment_sample.loc[experiment_id]
+original_selected_sample_ids = list(selected_mapping['ml_data_source'].values)
+
+selected_original_data = original_data.loc[original_selected_sample_ids]
+
+selected_original_data.head(10)
+
+
+# In[17]:
+
+
+# Want to get simulated samples associated with experiment_id
+# Since we sampled experiments with replacement, we want to find the first set of samples matching the experiment id
+match_experiment_id = ''
+for experiment_name in simulated_data_scaled_df['experiment_id'].values:
+    if experiment_name.split("_")[0] == experiment_id:
+        match_experiment_id = experiment_name 
+
+
+# In[18]:
+
+
+# Get simulated samples associated with experiment_id
+selected_simulated_data = simulated_data_scaled_df[simulated_data_scaled_df['experiment_id'] == match_experiment_id]
+
+# Map sample ids from original data to simulated data
+selected_simulated_data.index = original_selected_sample_ids
+selected_simulated_data = selected_simulated_data.drop(columns=['experiment_id'])
+
+selected_simulated_data.head(5)
+
+
+# In[19]:
+
+
+# Save selected samples
+# This will be used as input into R script to identify differentially expressed genes
+selected_simulated_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "selected_simulated_data_"+experiment_id+"_example.txt")
+
+selected_original_data_file = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "selected_original_data_"+experiment_id+"_example.txt")
+
+selected_simulated_data.to_csv(
+        selected_simulated_data_file, float_format='%.3f', sep='\t')
+
+selected_original_data.to_csv(
+        selected_original_data_file, float_format='%.3f', sep='\t')
+
+
+# ## Generate multiple simulated experiments 
+# 
+# Generate different simulated datasets using the same E-GEOD-51409 template experiment and shifting the experiment in the linear space in different directions multiple times
+
+# In[20]:
+
+
+# Generate multiple simulated datasets
+for i in range(num_runs):
+    generate_labeled_data.shift_template_experiment(
+        normalized_data_file,
+        experiment_id,
+        NN_architecture,
+        dataset_name,
+        local_dir,
+        base_dir,
+        i)
+
