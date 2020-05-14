fafbb61e9f8dbd75b033b084814337338d6b544f
ajlee21 <alexjlee.21@gmail.com>
2020-05-13 21:38:11 -0400

    add scripts

---
+++ Rank_pathways/nbconverted/1_process_data.py
@@ -0,0 +1,196 @@
+
+# coding: utf-8
+
+# # Process data
+# This notebook does the following:
+# 
+# 1. Selects template experiment
+# 2. Downloads subset of recount2 data, including the template experiment (50 random experiments + 1 template experiment)
+# 3. Train VAE on subset of recount2 data
+
+# In[11]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('load_ext', 'rpy2.ipython')
+get_ipython().run_line_magic('autoreload', '2')
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+import random
+import rpy2
+import seaborn as sns
+from sklearn import preprocessing
+import pickle
+
+sys.path.append("../")
+from functions import generate_labeled_data, utils, pipeline
+
+from numpy.random import seed
+randomState = 123
+seed(randomState)
+
+
+# In[12]:
+
+
+# Read in config variables
+base_dir = os.path.abspath(os.path.join(os.getcwd(),"../"))
+
+config_file = os.path.abspath(os.path.join(base_dir,
+                                           "Rank_pathways",
+                                           "init_config.tsv"))
+params = utils.read_config(config_file)
+
+
+# ### Select template experiment
+# 
+# We manually selected bioproject [SRP000762](https://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP000762), which contains 4 A549 sample replicates - 2 were treated with 100nM Dexamethasone (DEX) and 2 were treated with 0.01% ethanol (control).
+# 
+# For each sample replicate there were 5-6 sequencing runs.
+# 
+# * [DEX treated replicate 1](https://www.ncbi.nlm.nih.gov/sra/SRX006804[accn])
+# * [DEX treated replicate 2](https://www.ncbi.nlm.nih.gov/sra/SRX006806[accn])
+# * [Ethanol treated replicate 1](https://www.ncbi.nlm.nih.gov/sra/SRX006805[accn])
+# * [Ethanol treated replicate 2](https://www.ncbi.nlm.nih.gov/sra/SRX006807[accn])
+# 
+# For this analysis we will treat each run as a *sample*
+
+# In[3]:
+
+
+# User selected experiment id
+project_id = "SRP000762"
+
+
+# In[13]:
+
+
+# Load params
+local_dir = params["local_dir"]
+dataset_name = params['dataset_name']
+NN_architecture = params['NN_architecture']
+
+
+# ### Download subset of recount2 to use as a compendium
+# The compendium will be composed of random experiments + the selected template experiment
+
+# In[5]:
+
+
+get_ipython().run_cell_magic('R', '', '# Select 59\n# Run one time\nif (!requireNamespace("BiocManager", quietly = TRUE))\n    install.packages("BiocManager")\nBiocManager::install("recount")')
+
+
+# In[6]:
+
+
+get_ipython().run_cell_magic('R', '', "library('recount')")
+
+
+# In[7]:
+
+
+get_ipython().run_cell_magic('R', '-i project_id -i base_dir -i local_dir', "\nsource('../functions/download_recount2_data.R')\n\nget_recount2_compendium(project_id, base_dir, local_dir)")
+
+
+# ### Download expression data for selected project id
+
+# In[8]:
+
+
+get_ipython().run_cell_magic('R', '-i project_id -i local_dir', "\nsource('../functions/download_recount2_data.R')\n\nget_recount2_template_experiment(project_id, local_dir)")
+
+
+# ### Normalize compendium 
+
+# In[6]:
+
+
+# Load real gene expression data
+original_compendium_file = os.path.join(
+    local_dir,
+    "recount2_compedium_data.tsv")
+
+
+# In[7]:
+
+
+# Read data
+original_compendium = pd.read_table(
+    original_compendium_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+print(original_compendium.shape)
+original_compendium.head()
+
+
+# In[8]:
+
+
+# 0-1 normalize per gene
+scaler = preprocessing.MinMaxScaler()
+original_data_scaled = scaler.fit_transform(original_compendium)
+original_data_scaled_df = pd.DataFrame(original_data_scaled,
+                                columns=original_compendium.columns,
+                                index=original_compendium.index)
+
+original_data_scaled_df.head()
+
+
+# In[9]:
+
+
+# Save normalized data
+normalized_data_file = os.path.join(
+    local_dir,
+    "normalized_recount2_compendium_data.tsv")
+
+original_data_scaled_df.to_csv(
+    normalized_data_file, float_format='%.3f', sep='\t')
+
+# Save scaler transform
+scaler_file = os.path.join(
+    local_dir,
+    "scaler_transform.pickle")
+
+outfile = open(scaler_file,'wb')
+pickle.dump(scaler,outfile)
+outfile.close()
+
+
+# ### Train VAE 
+
+# In[17]:
+
+
+# Setup directories
+# Create VAE directories
+output_dirs = [os.path.join(base_dir, dataset_name, "models"),
+               os.path.join(base_dir, dataset_name, "logs")]
+
+# Check if analysis output directory exist otherwise create
+for each_dir in output_dirs:
+    if os.path.exists(each_dir) == False:
+        print('creating new directory: {}'.format(each_dir))
+        os.makedirs(each_dir, exist_ok=True)
+
+# Check if NN architecture directory exist otherwise create
+for each_dir in output_dirs:
+    new_dir = os.path.join(each_dir, NN_architecture)
+    if os.path.exists(new_dir) == False:
+        print('creating new directory: {}'.format(new_dir))
+        os.makedirs(new_dir, exist_ok=True)
+
+
+# In[14]:
+
+
+# Train VAE on new compendium data
+# Write out model to rank_pathways directory
+pipeline.train_vae(config_file,
+                   normalized_data_file)
+
---
+++ Rank_pathways/nbconverted/2_simulate_data.py
@@ -0,0 +1,495 @@
+
+# coding: utf-8
+
+# # Simulate data
+# This notebook generates simulated experiments by sampling from the VAE using the user selected template experiment as a guide for sampling
+
+# In[1]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+import os
+import sys
+import glob
+import pandas as pd
+import numpy as np
+import random
+import seaborn as sns
+import umap
+from keras.models import load_model
+from sklearn.decomposition import PCA
+import pickle
+
+from plotnine import (ggplot,
+                      labs,  
+                      geom_line, 
+                      geom_point,
+                      geom_errorbar,
+                      aes, 
+                      ggsave, 
+                      theme_bw,
+                      theme,
+                      xlim,
+                      ylim,
+                      facet_wrap,
+                      scale_color_manual,
+                      guides, 
+                      guide_legend,
+                      element_blank,
+                      element_text,
+                      element_rect,
+                      element_line,
+                      coords)
+
+sys.path.append("../")
+from functions import utils, generate_labeled_data
+
+from numpy.random import seed
+randomState = 123
+seed(randomState)
+
+
+# In[2]:
+
+
+# Read in config variables
+base_dir = os.path.abspath(os.path.join(os.getcwd(),"../"))
+
+config_file = os.path.abspath(os.path.join(base_dir,
+                                           "Rank_pathways",
+                                           "init_config.tsv"))
+params = utils.read_config(config_file)
+
+
+# In[3]:
+
+
+# Load params
+local_dir = params["local_dir"]
+dataset_name = params['dataset_name']
+NN_architecture = params['NN_architecture']
+num_runs = 25
+
+NN_dir = os.path.join(
+    base_dir, 
+    dataset_name, 
+    "models", 
+    NN_architecture)
+
+# User selected experiment id
+project_id = "SRP000762"
+
+
+# In[4]:
+
+
+# Load real compendium
+compendium_file = os.path.join(
+    local_dir,
+    "recount2_compedium_data.tsv")
+
+normalized_compendium_file = os.path.join(
+    local_dir,
+    "normalized_recount2_compendium_data.tsv")
+
+# Load real template experiment
+template_data_file = os.path.join(
+    local_dir,
+    "recount2_template_data.tsv")
+
+# Load pickled files
+scaler_transform_file = os.path.join(
+    local_dir,
+    "scaler_transform.pickle")
+
+scaler = pickle.load(open(scaler_transform_file, "rb" ) )
+
+
+# In[5]:
+
+
+# Read data
+compendium = pd.read_table(
+    compendium_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+normalized_compendium = pd.read_table(
+    normalized_compendium_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+template_data = pd.read_table(
+    template_data_file,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+
+# ### Simulate experiments using selected template experiment
+
+# In[6]:
+
+
+# Simulate experiments
+# Make sure range is correct
+# Generate multiple simulated datasets
+#for i in range(num_runs):
+#    generate_labeled_data.shift_template_experiment(
+#        normalized_compendium_file,
+#        project_id,
+#        NN_architecture,
+#        dataset_name,
+#        scaler,
+#        local_dir,
+#        base_dir,
+#        i)
+
+
+# ### Quick validation of simulated experiments
+
+# **Spot check expression values**
+# 1. Values are different between different simulated data files (meaning it was a different simulated dataset), and different from the template experiment
+# 2. Range of values is scaled the same as the compendium
+
+# In[7]:
+
+
+# Compendium
+print(compendium.shape)
+compendium.head()
+
+
+# In[8]:
+
+
+sns.distplot(compendium['ENSG00000000003.14'])
+
+
+# In[9]:
+
+
+# Template experiment
+print(template_data.shape)
+template_data.head()
+
+
+# In[10]:
+
+
+sns.distplot(template_data['ENSG00000000003.14'])
+
+
+# In[11]:
+
+
+# Manual select one simulated experiment
+simulated_file_1 = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "selected_simulated_data_SRP000762_0.txt")
+
+# Read data
+simulated_test_1 = pd.read_table(
+    simulated_file_1,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+print(simulated_test_1.shape)
+simulated_test_1.head()
+
+
+# In[12]:
+
+
+sns.distplot(simulated_test_1['ENSG00000000003.14'])
+
+
+# In[55]:
+
+
+# Manual select another simulated experiment
+simulated_file_2 = os.path.join(
+    local_dir,
+    "pseudo_experiment",
+    "selected_simulated_data_SRP000762_10.txt")
+
+# Read data
+simulated_test_2 = pd.read_table(
+    simulated_file_2,
+    header=0,
+    sep='\t',
+    index_col=0)
+
+print(simulated_test_2.shape)
+simulated_test_2.head()
+
+
+# In[56]:
+
+
+sns.distplot(simulated_test_2['ENSG00000000003.14'])
+
+
+# **Check clustering of simulated samples**
+# 
+# Check UMAP of original experiment and simulated experiments. Expect to see a similar structure in the template and simulated experiments. Also expect to see that the simulated experiment follows the distribution of the compendium.
+
+# **Visualization in latent space**
+
+# In[57]:
+
+
+# Load VAE models
+model_encoder_file = glob.glob(os.path.join(
+        NN_dir,
+        "*_encoder_model.h5"))[0]
+
+weights_encoder_file = glob.glob(os.path.join(
+    NN_dir,
+    "*_encoder_weights.h5"))[0]
+
+model_decoder_file = glob.glob(os.path.join(
+    NN_dir,
+    "*_decoder_model.h5"))[0]
+
+weights_decoder_file = glob.glob(os.path.join(
+    NN_dir,
+    "*_decoder_weights.h5"))[0]
+
+# Load saved models
+loaded_model = load_model(model_encoder_file)
+loaded_decode_model = load_model(model_decoder_file)
+
+loaded_model.load_weights(weights_encoder_file)
+loaded_decode_model.load_weights(weights_decoder_file)
+
+
+# In[58]:
+
+
+pca = PCA(n_components=2)
+
+
+# In[59]:
+
+
+# Embedding of real compendium (encoded)
+
+# Scale compendium
+compendium_scaled = scaler.transform(compendium)
+compendium_scaled_df = pd.DataFrame(compendium_scaled,
+                                    columns=compendium.columns,
+                                    index=compendium.index)
+
+# Encode compendium into latent space
+compendium_encoded = loaded_model.predict_on_batch(compendium_scaled_df)
+
+compendium_encoded_df = pd.DataFrame(data=compendium_encoded, 
+                                     index=compendium.index)
+
+# Get and save model
+#model = umap.UMAP(random_state=randomState).fit(compendium_encoded_df)
+model = pca.fit(compendium_encoded_df)
+
+compendium_UMAPencoded = model.transform(compendium_encoded_df)
+
+compendium_UMAPencoded_df = pd.DataFrame(data=compendium_UMAPencoded,
+                                         index=compendium_encoded_df.index,
+                                         columns=['1','2'])
+
+# Add label
+compendium_UMAPencoded_df['experiment_id'] = 'background'
+
+
+# In[60]:
+
+
+# Embedding of real template experiment (encoded)
+
+# Scale template data
+template_data_scaled = scaler.transform(template_data)
+template_data_scaled_df = pd.DataFrame(template_data_scaled,
+                                    columns=template_data.columns,
+                                    index=template_data.index)
+
+# Encode template experiment into latent space
+template_encoded = loaded_model.predict_on_batch(template_data_scaled)
+template_encoded_df = pd.DataFrame(data=template_encoded,
+                                   index=template_data.index)
+
+template_UMAPencoded = model.transform(template_encoded_df)
+
+template_UMAPencoded_df = pd.DataFrame(data=template_UMAPencoded,
+                                         index=template_encoded_df.index,
+                                         columns=['1','2'])
+
+# Add back label column
+template_UMAPencoded_df['experiment_id'] = 'template_experiment'
+
+
+# In[61]:
+
+
+# Embedding of simulated experiment (encoded)
+
+# Scale simulated data
+simulated_test_2_scaled = scaler.transform(simulated_test_2)
+simulated_test_2_scaled_df = pd.DataFrame(simulated_test_2_scaled,
+                                          columns=simulated_test_2.columns,
+                                          index=simulated_test_2.index)
+
+# Encode simulated experiment into latent space
+simulated_encoded = loaded_model.predict_on_batch(simulated_test_2_scaled)
+simulated_encoded_df = pd.DataFrame(
+    simulated_encoded, index=simulated_test_2.index)
+
+simulated_UMAPencoded = model.transform(simulated_encoded_df)
+
+simulated_UMAPencoded_df = pd.DataFrame(data=simulated_UMAPencoded,
+                                         index=simulated_encoded_df.index,
+                                         columns=['1','2'])
+
+# Add back label column
+simulated_UMAPencoded_df['experiment_id'] = 'simulated_experiment'
+
+
+# In[62]:
+
+
+# Concatenate dataframes
+combined_UMAPencoded_df = pd.concat([compendium_UMAPencoded_df, 
+                                    template_UMAPencoded_df,
+                                    simulated_UMAPencoded_df])
+
+combined_UMAPencoded_df.shape
+
+
+# In[63]:
+
+
+# Plot
+fig = ggplot(combined_UMAPencoded_df, aes(x='1', y='2'))
+fig += geom_point(aes(color='experiment_id'), alpha=0.2)
+fig += labs(x ='PCA 1',
+            y = 'PCA 2',
+            title = 'PCA original data with experiments (latent space)')
+fig += theme_bw()
+fig += theme(
+    legend_title_align = "center",
+    plot_background=element_rect(fill='white'),
+    legend_key=element_rect(fill='white', colour='white'), 
+    legend_title=element_text(family='sans-serif', size=15),
+    legend_text=element_text(family='sans-serif', size=12),
+    plot_title=element_text(family='sans-serif', size=15),
+    axis_text=element_text(family='sans-serif', size=12),
+    axis_title=element_text(family='sans-serif', size=15)
+    )
+fig += guides(colour=guide_legend(override_aes={'alpha': 1}))
+fig += scale_color_manual(['#bdbdbd', 'red', 'blue'])
+fig += geom_point(data=combined_UMAPencoded_df[combined_UMAPencoded_df['experiment_id'] == 'template_experiment'],
+                  alpha=0.2, 
+                  color='blue')
+fig += geom_point(data=combined_UMAPencoded_df[combined_UMAPencoded_df['experiment_id'] == 'simulated_experiment'],
+                  alpha=0.2, 
+                  color='red')
+
+print(fig)
+
+
+# **Visualization in gene space**
+
+# In[64]:
+
+
+# Embedding of real compendium
+
+# Get and save model
+model = umap.UMAP(random_state=randomState).fit(compendium_scaled_df)
+
+compendium_UMAPencoded = model.transform(compendium_scaled_df)
+
+compendium_UMAPencoded_df = pd.DataFrame(data=compendium_UMAPencoded,
+                                         index=compendium_scaled_df.index,
+                                         columns=['1','2'])
+# Add label
+compendium_UMAPencoded_df['experiment_id'] = 'background'
+
+
+# In[65]:
+
+
+# Embedding of real template experiment
+
+template_UMAPencoded = model.transform(template_data_scaled_df)
+
+template_UMAPencoded_df = pd.DataFrame(data=template_UMAPencoded,
+                                         index=template_data_scaled_df.index,
+                                         columns=['1','2'])
+
+# Add back label column
+template_UMAPencoded_df['experiment_id'] = 'template_experiment'
+
+
+# In[66]:
+
+
+# Embedding of simulated template experiment
+
+simulated_UMAPencoded = model.transform(simulated_test_2_scaled_df)
+
+simulated_UMAPencoded_df = pd.DataFrame(data=simulated_UMAPencoded,
+                                         index=simulated_test_2_scaled_df.index,
+                                         columns=['1','2'])
+
+# Add back label column
+simulated_UMAPencoded_df['experiment_id'] = 'simulated_experiment'
+
+
+# In[67]:
+
+
+# Concatenate dataframes
+combined_UMAPencoded_df = pd.concat([compendium_UMAPencoded_df, 
+                                    template_UMAPencoded_df,
+                                    simulated_UMAPencoded_df])
+
+combined_UMAPencoded_df.shape
+
+
+# In[68]:
+
+
+# Plot
+fig = ggplot(combined_UMAPencoded_df, aes(x='1', y='2'))
+fig += geom_point(aes(color='experiment_id'), alpha=0.2)
+fig += labs(x ='UMAP 1',
+            y = 'UMAP 2',
+            title = 'UMAP original data with experiments (gene space)')
+fig += theme_bw()
+fig += theme(
+    legend_title_align = "center",
+    plot_background=element_rect(fill='white'),
+    legend_key=element_rect(fill='white', colour='white'), 
+    legend_title=element_text(family='sans-serif', size=15),
+    legend_text=element_text(family='sans-serif', size=12),
+    plot_title=element_text(family='sans-serif', size=15),
+    axis_text=element_text(family='sans-serif', size=12),
+    axis_title=element_text(family='sans-serif', size=15)
+    )
+fig += guides(colour=guide_legend(override_aes={'alpha': 1}))
+fig += scale_color_manual(['#bdbdbd', 'red', 'blue'])
+fig += geom_point(data=combined_UMAPencoded_df[combined_UMAPencoded_df['experiment_id'] == 'template_experiment'],
+                  alpha=0.2, 
+                  color='blue')
+fig += geom_point(data=combined_UMAPencoded_df[combined_UMAPencoded_df['experiment_id'] == 'simulated_experiment'],
+                  alpha=0.2, 
+                  color='red')
+
+print(fig)
+
