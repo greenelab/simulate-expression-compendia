646d3bad0c96af337eadff6779625bd01e93c4ac
ajlee21 <alexjlee.21@gmail.com>
2020-07-06 11:27:48 -0400

    remove local pytest script

--- tests.sh
+++
@@ -1,5 +0,0 @@
-# First manually 
-# conda activate simulate_expression_compendia
-# pip install git+https://github.com/ajlee21/ponyo.git@pypi
-pytest -v --nbval-lax --current-env Pseudomonas_tests/*.ipynb
-pytest -v --nbval-lax --current-env Human_tests/*.ipynb
